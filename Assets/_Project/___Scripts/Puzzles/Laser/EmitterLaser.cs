a19a0fc3e39fc1ad427f2ce6d81e2b2c26fac108
NattanGTech <nleguyader@gaming.tech>
2025-04-02 13:43:26 +0200

    [REF] Emitteur Laser

--- Assets/_Project/___Scripts/Puzzles/Laser/EmitterLaser.cs
+++ Assets/_Project/___Scripts/Puzzles/Laser/EmitterLaser.cs
@@ -2,63 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class EmitterLaser : MonoBehaviour
 {
-    private GameObject _cylinder;
+    private LineRenderer _laser;
     private Vector3 _startPosition;
-    private float _lastDistance;
     private int _layerMask;
     void Start()
     {
-        _cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-        _cylinder.transform.SetParent(transform);
-        _cylinder.transform.up = transform.right;
-        _cylinder.transform.localScale = new Vector3(0.3f, 0f, 0.3f);
-        _cylinder.layer = 2;
-
+        _laser = transform.AddComponent<LineRenderer>();
         _startPosition = transform.position;
         _layerMask = ~LayerMask.GetMask("Ignore Raycast");
 
-        ResetLaserDistance();
+        ResetLaser();
     }
 
     void Update()
     {
         if (Physics.Raycast(_startPosition, transform.right, out RaycastHit hit, 10f, _layerMask))
         {
-            float distance = Vector3.Distance(hit.point, _startPosition);
-            if (!Mathf.Approximately(distance, _lastDistance))
+            if (IsPositionAlreadyAdded(hit.point)) return;
+
+            ResetLaser();
+            AddLaserPoint(hit.point);
+        }
+        else if(_laser.positionCount > 0)
+        {
+            ResetLaser();
+        }
+    }
+
+    private void AddLaserPoint(Vector3 newPosition)
+    {
+        _laser.positionCount++;
+        _laser.SetPosition(_laser.positionCount - 1, newPosition);
+    }
+
+
+    private void ResetLaser()
+    {
+        _laser.positionCount = 0;
+        AddLaserPoint(_startPosition);
+    }
+
+    bool IsPositionAlreadyAdded(Vector3 position)
+    {
+        for (int i = 0; i < _laser.positionCount; i++)
+        {
+            if (_laser.GetPosition(i) == position)
             {
-                UpdateLaserDistance(distance);
+                return true;
             }
         }
-        else if (!Mathf.Approximately(0f, _lastDistance))
-        {
-            ResetLaserDistance();
-        }
-        //Debug.DrawRay(transform.position, transform.right * 10f, Color.red);
+        return false;
     }
 
-    private void UpdateLaserDistance(float distance)
-    {
-        _lastDistance = distance;
-
-        _cylinder.transform.localScale = new Vector3(
-            _cylinder.transform.localScale.x, 
-            (distance) * 0.5f, 
-            _cylinder.transform.localScale.z
-        );
-
-        _cylinder.transform.position = _startPosition + transform.right * (distance * 0.5f);
-    }
-
-    private void ResetLaserDistance()
-    {
-        UpdateLaserDistance(0f);
-
-        //_lastDistance = 0f;
-        //_cylinder.transform.localScale = new Vector3(0.3f, 0f, 0.3f);
-        //_cylinder.transform.position = _pointToStart.position;
-    }
 }

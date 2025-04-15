490f2642d698488e5bae8611bcae6ee8bbed516a
RomainGTech <rboisseau@gaming.tech>
2025-04-15 14:15:43 +0200

    [REF] Room1 final vine retraction implementation

--- Assets/_Project/___Scripts/Puzzles/Vine/VineRetraction.cs
+++ Assets/_Project/___Scripts/Puzzles/Vine/VineRetraction.cs
@@ -10,26 +10,40 @@ public class VineRetraction : MonoBehaviour
     private CapsuleCollider _boxCollider;
     private Material _mat;
 
+    private float _originalHeight;
+    private Vector3 _originalCenter;
+
     private void Start()
     {
         Renderer renderer = _vine.GetComponent<Renderer>();
         _boxCollider = _vine.GetComponent<CapsuleCollider>();
         _mat = renderer.material;
+        _originalHeight = _boxCollider.height;
+        _originalCenter = _boxCollider.center;
     }
 
-    private void OnTriggerEnter(Collider other)
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject == _box)
+    //    {
+
+    //    }
+    //}
+
+    private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject == _box)
+        if(other.gameObject == _box)
         {
-            Vector3 localEntryPos = _vine.transform.InverseTransformPoint(other.transform.position);
+            Vector3 localEntryPos = _vine.transform.InverseTransformPoint(_box.transform.position);
             float zLocal = localEntryPos.z;
-            float zMin = _boxCollider.center.z - _boxCollider.height * 0.5f;
-            float zMax = _boxCollider.center.z + _boxCollider.height * 0.5f;
-            float zRatio = Mathf.InverseLerp(zMin, zMax, zLocal);
+            float zMin = _originalCenter.z - _originalHeight * 0.5f;
+            float zMax = _originalCenter.z + _originalHeight * 0.5f;
+            float zRatio = Mathf.Clamp01(Mathf.InverseLerp(zMin, zMax, zLocal));
 
             float growValue = Mathf.Lerp(0f, 1f, zRatio);
             float height = Mathf.Lerp(1f, 13.5f, zRatio);
             float centerZ = Mathf.Lerp(-7f, 0f, zRatio);
+
             ChangeVineDatas(growValue, height, new Vector3(0, 0, centerZ));
         }
     }

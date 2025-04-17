902f308e65355aae7b9ced34ad0ac20a25db846b
RomainGTech <rboisseau@gaming.tech>
2025-04-17 20:59:39 +0200

    [ADD] Damier implementation refactored

--- Assets/_Project/___Scripts/Systems/BaseLevelManager/Floor1Room3LevelManager.cs
+++ Assets/_Project/___Scripts/Systems/BaseLevelManager/Floor1Room3LevelManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class Floor1Room3LevelManager : BaseLevelManager
 {
     private List<Cell> _brokenCells = new List<Cell>();
 
+    [Header("Damier")]
+    [SerializeField] private CinemachineVirtualCamera _damierCamera;
+
     public List<Cell> BrokenCells { get => _brokenCells; set => _brokenCells = value; }
+    public CinemachineVirtualCamera DamierCamera { get => _damierCamera; }
 
 }

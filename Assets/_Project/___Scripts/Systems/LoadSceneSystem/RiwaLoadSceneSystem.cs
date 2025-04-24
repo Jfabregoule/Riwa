ff394826f911945ab31719b564d54c5941766fd9
Julien Fabregoule <jfabregoule@gaming.tech>
2025-04-24 17:59:16 +0200

    [MERGE] Removed some useless Usings

--- Assets/_Project/___Scripts/Systems/LoadSceneSystem/RiwaLoadSceneSystem.cs
+++ Assets/_Project/___Scripts/Systems/LoadSceneSystem/RiwaLoadSceneSystem.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Diagnostics;
 
 [Serializable]
 public enum DoorDirection

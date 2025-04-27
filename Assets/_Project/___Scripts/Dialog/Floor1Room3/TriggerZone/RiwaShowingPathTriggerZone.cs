b0ba0255f52fbbc60ba52415ce7e667fc1c5a40a
Jfabregoule <jfabregoule@gaming.tech>
2025-04-27 13:25:22 +0200

    [MERGE] Fixed Merge

--- Assets/_Project/___Scripts/Dialog/Floor1Room3/TriggerZone/RiwaShowingPathTriggerZone.cs
+++ Assets/_Project/___Scripts/Dialog/Floor1Room3/TriggerZone/RiwaShowingPathTriggerZone.cs
@@ -1,21 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class RiwaShowingPathTriggerZone : Room3DialogTriggerZoneParent
+public class RiwaShowingPathTriggerZone : MonoBehaviour
 {
+
+    protected bool _isPlayerInArea = false;
+    protected Floor1Room3LevelManager _instance;
+
     private void Start()
     {
+        _instance = (Floor1Room3LevelManager)Floor1Room3LevelManager.Instance;
         GameManager.Instance.OnTimeChangeStarted += DialogueToCall;
     }
 
-    public override void DialogueToCall(EnumTemporality temporality)
+    public void DialogueToCall(EnumTemporality temporality)
     {
         if (_isPlayerInArea && temporality == EnumTemporality.Present)
         {
             Debug.Log("RiwaShowPathTriggerZone");
             DialogueSystem.Instance.BeginDialogue(_instance.TutorialRoom3Manager.Room3Dialogue[2]);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out ACharacter chara))
+        {
+            _isPlayerInArea = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out ACharacter chara))
+            _isPlayerInArea = false;
     }
 }

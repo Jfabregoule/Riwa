313578edb92ebb207d8dc8f6140b6cdb344a4446
NattanGTech <nleguyader@gaming.tech>
2025-04-28 02:42:18 +0200

    [ADD] Push, Pull, Rotate On Joystick

--- Assets/_Project/___Scripts/Input/VariableDynamicJoystick.cs
+++ Assets/_Project/___Scripts/Input/VariableDynamicJoystick.cs
@@ -1,3 +1,4 @@
+﻿using Cinemachine.Utility;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
@@ -7,8 +8,11 @@ public class VariableDynamicJoystick : MonoBehaviour
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private CanvasGroup _arrowCanvasGroup;
     [SerializeField] private RectTransform _background;
-    [SerializeField] private RectTransform _handle;   
-    
+    [SerializeField] private RectTransform _handle;
+    [SerializeField] private RectTransform[] _arrowsRect;
+
+    private Vector2[] _anchors;
+
     private ACharacter _character;
 
     void OnEnable()
@@ -33,7 +37,11 @@ public class VariableDynamicJoystick : MonoBehaviour
 
     private void Start()
     {
-        
+        _anchors = new Vector2[4];
+        _anchors[0] = new Vector2(0.5f, 1f);
+        _anchors[1] = new Vector2(0.5f, 0f);
+        _anchors[2] = new Vector2(0f, 0.5f);
+        _anchors[3] = new Vector2(1f, 0.5f);
     }
 
     private void OnTouchStarted(Vector2 position)
@@ -70,53 +78,92 @@ public class VariableDynamicJoystick : MonoBehaviour
     }
     private void SetCanvasGroup()
     {
-        Helpers.EnabledCanvasGroup(_arrowCanvasGroup);
+        Vector3 caissePosition = _character.HoldingObject.transform.position;
+        Vector3 playerPosition = _character.transform.position;
 
-        Vector2 joystickDir = _character.InputManager.GetMoveDirection();
+        // Calcul de la direction entre le joueur et la caisse
+        Vector3 directionToCaisse = (caissePosition - playerPosition).normalized;
 
-        if (joystickDir == Vector2.zero) return;
-
-        //Pour calculer avec l'orientation de la cam�ra
-
+        // Récupère la direction de la caméra
         Vector3 camForward = Vector3.ProjectOnPlane(GameManager.Instance.CameraHandler.transform.forward, Vector3.up).normalized;
         Vector3 camRight = Vector3.Cross(Vector3.up, camForward);
 
-        /////////
+        // Direction actuelle du joueur
+        Vector3 playerForward = _character.transform.forward;
 
-        Vector3 inputDir = (camForward * joystickDir.y + camRight * joystickDir.x).normalized;
-
-        Vector3 worldForward = _character.transform.forward;
-        Vector3 worldRight = _character.transform.right;
-
-        float dotForward = Vector3.Dot(worldForward, inputDir);
-        float dotRight = Vector3.Dot(worldRight, inputDir);
+        // Calcul des valeurs de dot pour la direction du joueur et la caisse
+        float dotForward = Vector3.Dot(directionToCaisse, playerForward); // Direction relative à la vue du joueur
+        float dotRight = Vector3.Dot(directionToCaisse, camRight);
 
         if (Mathf.Abs(dotForward) > Mathf.Abs(dotRight))
         {
-            if (!_character.HoldingObject.TryGetComponent(out IMovable movable)) return;
             if (dotForward > 0.5f)
             {
-                //Pull
+                // Pull en haut
+                SetAnchor(1, 0);
 
+                // Push en bas
+                SetAnchor(0, 1);
+
+                // Rotation (Droite à Gauche)
+                SetAnchor(3, 2);
+
+                SetAnchor(2, 3);
             }
             else
             {
-                //Push
+                // Push en haut
+                SetAnchor(0, 0);
+
+                // Pull en bas
+                SetAnchor(1, 1);
+
+                // Rotation (Gauche à Droite)
+                SetAnchor(2, 2);
+
+                SetAnchor(3, 3);
             }
         }
         else
         {
-            if (!_character.HoldingObject.TryGetComponent(out IRotatable rotatable)) return;
             if (dotRight > 0.5f)
             {
-                //Rotate Droite
+                // Pull à droite
+                SetAnchor(1, 3);
+
+                // Push à gauche
+                SetAnchor(0, 2);
+
+                // Rotation (Gauche en bas)
+                SetAnchor(2, 1);
+
+                // Rotation (Droite en haut)
+                SetAnchor(3, 0);
             }
             else
             {
-                //Rotate Gauche
+                // Pull à gauche
+                SetAnchor(1, 2);
 
+                // Push à droite
+                SetAnchor(0, 3);
+
+                // Rotation (Gauche en haut)
+                SetAnchor(2, 0);
+
+                // Rotation (Droite en bas)
+                SetAnchor(3, 1);
             }
         }
+
+        Helpers.EnabledCanvasGroup(_arrowCanvasGroup);
+    }
+
+    private void SetAnchor(int idRect, int Direction)
+    {
+        _arrowsRect[idRect].anchorMax = _anchors[Direction];
+        _arrowsRect[idRect].anchorMin = _anchors[Direction];
+        _arrowsRect[idRect].anchoredPosition = _anchors[Direction];
     }
 
     private void DisableCanvasGroup()

090999edc5c35d982688f18a42b1c21f8f7a46ab
NattanGTech <nleguyader@gaming.tech>
2025-06-18 11:45:29 +0200

    [Fix] fixs to build

--- Assets/_Project/___Scripts/UI/HighlightEffect.cs
+++ Assets/_Project/___Scripts/UI/HighlightEffect.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph;
 using UnityEngine;
-using static Unity.VisualScripting.Member;
 
 public class HighlightEffect : MonoBehaviour
 {

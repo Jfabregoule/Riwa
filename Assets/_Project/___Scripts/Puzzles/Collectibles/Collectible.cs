1f87ef0d7c7a927940dbd168ad33f857a24c4715
RomainGTech <rboisseau@gaming.tech>
2025-06-18 13:36:34 +0200

    [REF] Collectible pickUp save

--- Assets/_Project/___Scripts/Puzzles/Collectibles/Collectible.cs
+++ Assets/_Project/___Scripts/Puzzles/Collectibles/Collectible.cs
@@ -24,7 +24,6 @@ public class Collectible : MonoBehaviour
 
     private void OnDisable()
     {
-        SaveSystem.Instance.SaveElement<bool>(_roomPrefix + "CollectiblePickUp", _taken);
         SaveSystem.Instance.OnLoadProgress -= LoadData;
     }
 
@@ -41,6 +40,7 @@ public class Collectible : MonoBehaviour
             DialogueSystem.Instance.BeginDialogue(_collectibleDialogue);
             GameManager.Instance.CollectibleManager.AddCollectible(1);
             _taken = true;
+            SaveSystem.Instance.SaveElement<bool>(_roomPrefix + "CollectiblePickUp", _taken);
             Destroy(gameObject);
         }
     }
